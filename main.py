cb3f995c1215da0c541499c3906a9d98422d1f73
Van Tu <nguyenvantu28011998it@gmail.com>
2022-03-19 04:11:51 +0700

    change port to deploy

--- main.py
+++ main.py
@@ -386,4 +386,4 @@ def ping():
 #     os.remove(filePath)
 #     return {"deleted":1}
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
